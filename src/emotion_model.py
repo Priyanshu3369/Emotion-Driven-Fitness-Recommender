9c7e889e0e013227a755ccf04bbf2861991bf828
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-05-03 08:32:56 +0530

    basic model training done

---
+++ src/emotion_model.py
@@ -0,0 +1,28 @@
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+
+
+def prepare_data(df):
+    X = df['clean_text']       # cleaned input text
+    y = df['emotion']          # emotion label
+
+    vectorizer = TfidfVectorizer()
+    X_vec = vectorizer.fit_transform(X)
+
+    return X_vec, y, vectorizer
+
+
+
+def train_emotion_classifier(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    print("Accuracy: ", accuracy_score(y_test, y_pred))
+    print("Classification Report:\n", classification_report(y_test, y_pred))
+
+    return model
